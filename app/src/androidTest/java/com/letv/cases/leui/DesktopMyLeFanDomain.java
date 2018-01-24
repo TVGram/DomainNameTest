032319dfaa44f309181fecb59bc311c924c16deb
ext-yuren.zhang <ext-yuren.zhang@le.com>
2018-01-24 12:02:29 +0800

    pc7

--- app/src/androidTest/java/com/letv/cases/leui/DesktopMyLeFanDomain.java
+++ app/src/androidTest/java/com/letv/cases/leui/DesktopMyLeFanDomain.java
@@ -16,73 +16,6 @@ import java.util.regex.Pattern;
 public class DesktopMyLeFanDomain extends LetvTestCase{
     int count=0;
 
-    @Test
-    @CaseName("我的乐范个人帐号和全部特权遍历")
-    public void testmyLeFan()throws UiObjectNotFoundException,RemoteException{
-        addStep("进入我的乐范");
-        gotoHomeScreen("我的乐范");
-
-            try {
-                mylefan();
-            } catch (Exception e) {
-                try {
-                    count++;
-                    failCount(count, getIntParams("Loop"), e.getMessage());
-                    gotoHomeScreen("我的乐范");
-                    mylefan();
-                } catch (RuntimeException re) {
-                    screenShot();
-                    Assert.fail(re.getMessage());
-                }
-            }
-
-    }
-    public void mylefan()throws UiObjectNotFoundException,RemoteException{
-        press_down(1);
-        sleepInt(1);
-        UiObject2 presonPrivilege=waitForObj(By.clazz("android.widget.FrameLayout").res("com.stv.plugin.ucenter:id/focus_layout"));
-        check("未进入个人特权",presonPrivilege !=null);
-        presonPrivilege.click();
-        presonPrivilege.click();
-        press_left(2);
-        UiObject2 allPrivilege=phone.findObject(By.text("全部特权"));
-        check("未进入全部特权",allPrivilege!=null);
-        String arrprivilege[]={"福利特权","购物特权","续费特权","金融特权","服务特权","游戏特权"};
-        for (int j = 1; j < arrprivilege.length; j++) {
-            press_left(3);
-            UiObject2 priv = waitForObj(By.clazz("android.widget.RadioButton").text(arrprivilege[j]));
-            check("未进入" + arrprivilege[j], priv != null);
-            press_down(1);
-            press_right(1);
-            press_center(1);
-            sleepInt(3);
-            press_back(1);
-            sleepInt(1);
-
-            press_right(1);
-            press_center(1);
-            sleepInt(3);
-            press_back(1);
-            sleepInt(1);
-
-        }
-        press_left(2);
-        press_up(7);
-        press_right(1);
-        press_center(1);
-        sleepInt(3);
-        press_back(1);
-        sleepInt(1);
-
-        press_right(1);
-        press_center(1);
-        sleepInt(3);
-        press_back(1);
-        sleepInt(1);
-
-
-        press_back(3);
-    }
 
     @Test
     @CaseName("进入桌面管理调整桌面我的乐范")
@@ -156,6 +89,76 @@ public class DesktopMyLeFanDomain extends LetvTestCase{
         }
 
     }
+
+
+    @Test
+    @CaseName("我的乐范全部特权遍历海报")
+    public void testmyLeFan()throws UiObjectNotFoundException,RemoteException{
+        addStep("进入我的乐范");
+        gotoHomeScreen("我的乐范");
+
+        try {
+            mylefan();
+        } catch (Exception e) {
+            try {
+                count++;
+                failCount(count, getIntParams("Loop"), e.getMessage());
+                gotoHomeScreen("我的乐范");
+                mylefan();
+            } catch (RuntimeException re) {
+                screenShot();
+                Assert.fail(re.getMessage());
+            }
+        }
+
+    }
+    public void mylefan()throws UiObjectNotFoundException,RemoteException{
+        press_down(1);
+        sleepInt(1);
+        UiObject2 presonPrivilege=waitForObj(By.clazz("android.widget.FrameLayout").res("com.stv.plugin.ucenter:id/focus_layout"));
+        check("未进入个人特权",presonPrivilege !=null);
+        presonPrivilege.click();
+        presonPrivilege.click();
+        press_left(2);
+        UiObject2 allPrivilege=phone.findObject(By.text("全部特权"));
+        check("未进入全部特权",allPrivilege!=null);
+        String arrprivilege[]={"福利特权","购物特权","续费特权","金融特权","服务特权","游戏特权"};
+        for (int j = 1; j < arrprivilege.length; j++) {
+            press_left(3);
+            UiObject2 priv = waitForObj(By.clazz("android.widget.RadioButton").text(arrprivilege[j]));
+            check("未进入" + arrprivilege[j], priv != null);
+            press_down(1);
+            press_right(1);
+            press_center(1);
+            sleepInt(3);
+            press_back(1);
+            sleepInt(1);
+
+            press_right(1);
+            press_center(1);
+            sleepInt(3);
+            press_back(1);
+            sleepInt(1);
+
+        }
+        press_left(2);
+        press_up(7);
+        press_right(1);
+        press_center(1);
+        sleepInt(3);
+        press_back(1);
+        sleepInt(1);
+
+        press_right(1);
+        press_center(1);
+        sleepInt(3);
+        press_back(1);
+        sleepInt(1);
+
+
+        press_back(3);
+    }
+
 
     @Test
     @CaseName("我的乐范海报")
